b43ead8079dfa0b49c295be91e0a01a4f593a2e4
utsavized <utsavpandey@gmail.com>
2018-12-07 16:53:30 -0800

    Add Tests project

---
+++ TeamsLogger.Tests/Mocks/TeamsWebhookClientMock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TeamsLogger.Tests.Mocks
+{
+    public class TeamsWebhookClientMock : ITeamsWebhookClient
+    {
+        public void Post(string jsonPayload)
+        {
+        }
+
+        public Task PostAsync(string jsonPayload)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TeamsLogger.Tests/TeamsLoggerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TeamsLogger.Models;
+
+namespace TeamsLogger.Tests
+{
+    [TestClass]
+    public class TeamsLoggerTests
+    {
+        [TestMethod]
+        public void CardIsCreated()
+        {
+            var logger = new TeamsLogger(
+                new TeamsWebhookClient("http://yourcompany.com/teams/channel/randomId"),
+                new LoggerConfiguration { AutomaticallySetColor = true },
+                "ModuleName");
+
+            logger.BeginRunningLog("This is title");
+            
+        }
+    }
+}
