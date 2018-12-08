a52f63bec6f0acaa4c0935ab31826e33346c03e2
utsavized <utsavpandey@gmail.com>
2018-12-07 16:53:38 -0800

    Add console usage

--- TeamsLogger.Console/Program.cs
+++ TeamsLogger.Console/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using TeamsLogger.Models;
 
 namespace TeamsLogger.Console
 {
@@ -10,6 +7,22 @@ namespace TeamsLogger.Console
     {
         static void Main(string[] args)
         {
+            var logger = new TeamsLogger(
+                new TeamsWebhookClient("http://yourcompany.com/teams/channel/randomId"),
+                new LoggerConfiguration { AutomaticallySetColor = true },
+                "");
+
+            logger.LogMessage(LogSeverity.Info, "Message to log");
+            logger.LogMessage(LogSeverity.Warn, "Log message", "CCDDC4");
+            logger.BeginRunningLog("Begin logging");
+            logger.AddNewSection();
+            logger.AddSubSectionActivityWithSeverity(LogSeverity.Info, "Some event happened");
+            logger.AddSubSectionActivityWithSeverity(LogSeverity.Warn, "Not so good event happened");
+            logger.AddSubSectionActivityWithSeverity(LogSeverity.Error, "Bad event happened");
+            var ex = new Exception("Bad stuff");
+            logger.AddNewSection(LogSeverity.Error, typeof(Exception).ToString(), ex.Message, $"```{ex.StackTrace}```", null, null, true);
+            logger.AddSubSectionActivityWithSeverity(LogSeverity.Info, "Some event happened");
+            logger.PostRunningLog();
         }
     }
 }
